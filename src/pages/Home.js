01fea21441aa2c6b1c476426a07d9ddabc4900ba
BOOBALAN S <boobalansaravanan0810@gmail.com>
2025-03-19 12:48:12 +0530

    commit

---
+++ src/pages/Home.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+import p1 from "./Assets/cover.jpeg";
+
+function Home() {
+    return (
+        <div className="home">
+            <nav className="navbar">
+                <div className="logo">
+                    <Link to="/" className="logo-name">EDGECUT</Link>
+                </div>
+                <div className="nav-links">
+                    <ul>
+                        <li><Link to="/">HOME</Link></li>
+                        <li><Link to="/about">ABOUT</Link></li>
+                        <li><Link to="/furnitures">FURNITURES</Link></li>
+                        <li><Link to="/blog">BLOG</Link></li>
+                        <li><Link to="/contact">CONTACT</Link></li>
+                        <li><Link to="/login">LOGIN</Link></li>
+
+                    </ul>
+                </div>
+            </nav>
+
+            <div className="home-content">
+                <div className="content-text">
+                    <h1>FIND YOUR ALL FURNITURES NEEDS</h1>
+                    <p>A furniture shop is a treasure trove of possibilities, showcasing a diverse range of pieces that cater to comfortf</p>
+                    <div className="btn-shop">
+                    <Link to="/About" className="btn-about">About Us</Link>
+                    <Link to="/contact" className="btn-contact">contact us</Link>
+                    </div>
+                </div>
+                <div className="content-image">
+                    <img src={p1} alt="home" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/pages/Login.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Login.css"; 
+
+function Login() {
+    return (
+        <div className="login">
+            <div className="login-header">
+                <h1>Login</h1>
+            </div>
+            <div className="login-content">
+                <form className="login-form">
+                    <input type="text" placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+                    <h3>Forget Password?</h3>
+                    <button type="submit">Login</button>
+                </form>
+            </div>           
+        </div>
+    );
+}
+
+export default Login;
